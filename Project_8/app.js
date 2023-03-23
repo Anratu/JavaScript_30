6016910804c69b4637532872e06b0abafb40ae52
Anratu <127898931+Anratu@users.noreply.github.com>
2023-03-23 14:35:21 -0500

    Update app.js

--- Project_8/app.js
+++ Project_8/app.js
@@ -48,3 +48,9 @@ canvas.addEventListener('mousedown', (e) => {
 });
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
+canvas.addEventListener('touchstart', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('touchmove', draw);
+canvas.addEventListener('touchend', () => isDrawing = false);
